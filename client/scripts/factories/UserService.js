963a1446aa90f4a0a76f4b75e062e6977cd463bd
Craig Baird <bairdcraig@gmail.com>
2017-09-07 20:58:50 -0500

    progress

--- client/scripts/factories/UserService.js
+++ client/scripts/factories/UserService.js
@@ -9,11 +9,9 @@ myApp.factory('UserService', ['$http', '$location', function($http, $location){
     getuser : function(){
       $http.get('/user').then(function(response) {
           if(response.data.username) {
-              // user has a current session on the server
               userObject.userName = response.data.username;
-              // console.log('User Data: ', userObject.userName);
+              console.log('User Data: ', userObject.userName);
           } else {
-              // user has no session, bounce them back to the login page
               $location.path('/home');
           }
       });
