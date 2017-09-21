909e086fa52a1de7c1b63802ae0e81f5e5b25a5d
Craig Baird <bairdcraig@gmail.com>
2017-09-20 20:50:07 -0500

    progress on search feature

--- client/scripts/controllers/myBookShelfController.js
+++ client/scripts/controllers/myBookShelfController.js
@@ -1,10 +1,11 @@
 myApp.controller('MyBookShelfController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
   console.log('MyBookShelfController loaded');
   $scope.logout = UserService.logout;
+  $scope.search = {};
 
-  $scope.searchForBook = function(input) {
-      console.log("searching for book " + input);
-      var book = input.name;
+  $scope.searchForBook = function(searchFromUser) {
+      console.log("searching for  ", searchFromUser);
+      var book = search.name;
   };
 
   // connect controller data to factory data here
