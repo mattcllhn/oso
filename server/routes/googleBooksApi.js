150af7e7293183732ccbd0aea84be7bec7c00f1a
Craig Baird <bairdcraig@gmail.com>
2017-09-30 20:19:53 -0500

    progress

--- server/routes/googleBooksApi.js
+++ server/routes/googleBooksApi.js
@@ -5,13 +5,9 @@ var path = require("path");
 // API call to get images and id for recipes
 router.get("/:books", function(req, res){
   var books = req.params.books;
-  // $http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients"+
-  //           "?fillIngredients=false&ingredients=" + ingredients + "&limitLicense=true&number=8&ranking=1")
-  // .header("X-Mashape-Key", "5odaehKxNbmshul9AvfFy6EBdgBXp1Dcw1rjsnR8zfBgCuY9mH")
-  // .header("Accept", "application/json")
-  $http.get("www.googleapis.com/books/v1/volumes?q=isbn:$isbn")
-  .end(function (result) {
-  res.send(result.body);
+      $http.get("www.googleapis.com/books/v1/volumes?q=isbn:$isbn")
+      .end(function (result) {
+      res.send(result.body);
   }); // end unirest.get
 });
 
