2a560883e1c615c41c67ccf5f74fb04501112c62
Craig Baird <bairdcraig@gmail.com>
2017-10-04 14:31:24 -0500

    progress on api

--- server/routes/googleBooksApi.js
+++ server/routes/googleBooksApi.js
@@ -7,12 +7,52 @@ router.get('/:isbn', function(req, res){
   var isbn = req.params.isbn;
   console.log('in /isbn in googleBooksApi.js', isbn);
       https.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
-    //   https.get('https://www.googleapis.com/books/v1/volumes?q=' + isbn)
         .end(function (result) {
-        // console.log(result.status, result.headers, result.body);
-        console.log(result.kind, result.items, result.totalItems);
-        res.send(result.body);
+        console.log(result.items[0]);
+        res.send(result.items[0]);
   }); // end http.get
 });
 
 module.exports = router;
+
+
+
+
+// router.get('/:isbn', function(req, res){
+//   var isbn = req.params.isbn;
+//   console.log('in /isbn in googleBooksApi.js', isbn);
+//       https.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
+//     //   https.get('https://www.googleapis.com/books/v1/volumes?q=' + isbn)
+//         .end(function (result) {
+//             var results = JSON.parse(result);
+//             if (results.totalItems) {
+//
+//               // There'll be only 1 book per ISBN
+//               var book = results.items[0];
+//
+//               var title = book.volumeInfo.title;
+//               var subtitle = book.volumeInfo.subtitle;
+//               var authors = book.volumeInfo.authors;
+//               var printType = book.volumeInfo.printType;
+//               var pageCount = book.volumeInfo.pageCount;
+//               var publisher = book.volumeInfo.publisher;
+//               var publishedDate = book.volumeInfo.publishedDate;
+//               var webReaderLink = book.accessInfo.webReaderLink;
+
+            //   var title = (book["volumeInfo"]["title"]);
+            //   var subtitle = (book["volumeInfo"]["subtitle"]);
+            //   var authors = (book["volumeInfo"]["authors"]);
+            //   var printType = (book["volumeInfo"]["printType"]);
+            //   var pageCount = (book["volumeInfo"]["pageCount"]);
+            //   var publisher = (book["volumeInfo"]["publisher"]);
+            //   var publishedDate = (book["volumeInfo"]["publishedDate"]);
+            //   var webReaderLink = (book["accessInfo"]["webReaderLink"]);
+
+              // For debugging
+//               console.log(book);
+//
+//             }
+//   }); // end http.get
+// });
+//
+// module.exports = router;
